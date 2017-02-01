8418850b6d34f8deefbb6c7d404f6462938b758c
Lyudmyla <kaluzhynova@gmil.com>
2017-02-01 12:37:31 +0200

    archives

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,13 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    years = [2015, 2016, 2017]
+    year = request.GET.get('year', None)
+    if year:
+        posts = Post.objects.filter(published_date__year=year).order_by('published_date')
+    else:
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts, 'years': years})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
