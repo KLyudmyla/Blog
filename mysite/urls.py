805080067c035c05fd14c3312f61387cbbdb4fbf
Lyudmyla <kaluzhynova@gmail.com>
2017-02-05 23:23:33 +0200

    edit-delete post

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,28 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+#import numpy as np
+#import matplotlib.pyplot as plt
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+#def graph(request):
+#    data = [33, 25, 20, 12, 10]
+#    plt.figure(num=1, figsize=(6, 6))
+#    plt.axes(aspect=1)
+#    plt.title('Plot 3', size=14)
+#    plt.pie(data, labels=('Group 1', 'Group 2', 'Group 3', 'Group 4', 'Group 5'))
+
+#    plt.savefig('static/images/1.png', format='png')   
+#    return render(request, "graph/graph1.html",)
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+#    url(r'^graph/', graph, name='graph'),
     url(r'', include('blog.urls')),
 ]
+
+
+
+
+
